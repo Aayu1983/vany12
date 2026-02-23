5a64f0b440ce47bef04de9fa8ab2f766dd2f9f11
Aayu1983 <aayusinghal1983@gmail.com>
2026-02-23 17:26:35 +0530

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg block" {
+resource "azurerm_resource_group" "rg_block" {
     for_each = var.Rg
     name     = each.value.name
     location = each.value.location
